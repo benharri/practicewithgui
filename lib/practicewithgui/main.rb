832e2edde5458427a8a6d532d317aa04aa1e867b
Dan <dkellogg@hagerty.com>
2017-12-21 10:33:16 -0500

    Answered the question of life

--- lib/practicewithgui/main.rb
+++ lib/practicewithgui/main.rb
@@ -1,7 +1,7 @@
 module practicewithgui
   CONST = 42
   def practicewithgui.greet
-    puts "#{CONST} is the answer to life, the universe, and everything"
+    puts "#{CONST} is the answer to life, the universe, and almost everything because only Yoda knew everything"
   end
 
 end
