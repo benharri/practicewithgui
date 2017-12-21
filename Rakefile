d603826383c958bb09835c1db7ee077802d6571c
Ben Harris <bharris@hagerty.com>
2017-12-21 10:21:56 -0500

    add rake tasks

---
+++ Rakefile
@@ -0,0 +1,5 @@
+task default: %w[start]
+
+task :start do
+  puts "hi from rake"
+end
---
+++ lib/practicewithgui/main.rb
@@ -0,0 +1,7 @@
+module practicewithgui
+  CONST = 42
+  def practicewithgui.greet
+    puts "#{CONST} is the answer to life, the universe, and everything"
+  end
+
+end
